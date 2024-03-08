c3f17d091e37799dd25b33dcfdc75adbb7276ffb
Angelillo15 <contact@angelillo15.es>
2024-03-08 21:51:03 +0100

    Add new modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,7 @@ rootProject.name = "NookCore"
 include("NookCore-Database")
 include("NookCore-Logger")
 include("NookCore-Core")
+include("NookCore-Config")
+include("NookCore-Event")
+include("NookCore-Messenger")
+include("NookCore-Player")
