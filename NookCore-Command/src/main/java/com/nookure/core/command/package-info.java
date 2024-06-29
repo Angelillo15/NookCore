08c8fbe9dbdd17110a3c0028a129e7e9aea7ded5
Angelillo15 <contact@angelillo15.es>
2024-06-29 18:54:43 +0200

    Add Command package-info.java

---
+++ NookCore-Command/src/main/java/com/nookure/core/command/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the command system for the NookCore core.
+ * See {@link com.nookure.core.command.Command} for more information.
+ *
+ * @since 1.0.0
+ */
+package com.nookure.core.command;
